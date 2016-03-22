70c8db4c0bddc8d3377e0ae6f28c4f4571bbbb72
Patrick Multhaup <pjmulthaup@gmail.com>
2016-03-22 16:31:51 -0700

    commands and events seperate into modules, both loaded into Shell. Initilization inheretence added for additionial local variables

--- shell_bot.rb
+++ shell_bot.rb
@@ -1,15 +1,45 @@
 require 'discordrb'
  
-shell = Discordrb::Bot.new ENV["DISCORD_EMAIL"].to_str, ENV["DISCORD_PASS"].to_str
- 
-#bot = Discordrb::Commands::CommandBot.new  ENV["DISCORD_EMAIL"].to_str, ENV["DISCORD_PASS"].to_str
- 
-start_time = Time.now
- 
-shell.message(with_text: "Ping!") do |event|
-  event.respond "Pong!"
+class Shell < Discordrb::Commands::CommandBot
+  def initialize(email, password, prefix, attributes = {}, debug = false)
+    super(email, password, prefix, attributes = {}, debug = false)
+    @start_time = Time.now
+  end
 end
- 
+
+module AnnouncerEvents
+  extend Discordrb::EventContainer
+
+  message(with_text: "hi shell!") do |event|
+    event.respond "Hi, #{event.user.name}!"
+  end
+
+  member_join do |event|
+    event.server.general_channel.send_message "#{event.user.name} joined!"
+  end
+
+  message(with_text: "!btc") do |event|
+    event.respond "Current Bitcoin Price: #{run_py('btc.py', '')}."
+  end
+end
+
+module UtilityCommands
+  extend Discordrb::Commands::CommandContainer
+
+  command :my_id do |event|
+    event.user.id
+  end
+
+  command :uptime do |event|
+    "I have been running for #{uptime(@start_time)}."
+  end
+end
+
+shell = Shell.new(ENV["DISCORD_EMAIL"].to_str, ENV["DISCORD_PASS"].to_str, '!')
+shell.include! AnnouncerEvents
+shell.include! UtilityCommands
+#bot = Discordrb::Commands::CommandBot.new  ENV["DISCORD_EMAIL"].to_str, ENV["DISCORD_PASS"].to_str
+
 def uptime(start_time)
   Time.now - start_time
 end
@@ -20,17 +50,11 @@ def run_py(scriptname, parameters)
     return shell_return
 end
  
-shell.message(with_text: "!uptime") do |event|
-  event.respond "I have been running for #{uptime(start_time)}."
-end
  
-shell.message(with_text: "!btc") do |event|
-  event.respond "Current Bitcoin Price: #{run_py('btc.py', '')}."
-end
  
 #bot.command :up do |_event, ip
 #  event.respond "#{run_py('ip.py', ip)}."
 #end
  
 shell.run
-#bot.run
+
