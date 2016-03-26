8f5cdc9e9e3402b31a873cd47854909807d3f315
Patrick Multhaup <pjmulthaup@gmail.com>
2016-03-25 17:41:59 -0700

    seperated modules into files

--- shell_bot.rb
+++ shell_bot.rb
@@ -1,49 +1,11 @@
 require 'discordrb'
+Dir[File.dirname(__FILE__) + '/lib/*.rb'].each {|file| require file }
+# require_relative '/ruby_modules/announcer_events'
  
 class Shell < Discordrb::Commands::CommandBot
   # This is doing absolutely nothing
   def initialize(email, password, prefix, attributes = {}, debug = false)
     super(email, password, prefix, attributes = {}, debug = false)
-  end
-end
-
-module AnnouncerEvents
-  extend Discordrb::EventContainer
-
-  def self.run_py(scriptname, parameters)
-      exec_str = 'python ' + 'py/'+scriptname + ' ' + parameters
-      shell_return = %x(#{exec_str})
-      return shell_return
-  end
-
-  message(with_text: "hi shell!") do |event|
-    event.respond "Hi, #{event.user.name}!"
-  end
-
-  member_join do |event|
-    event.server.general_channel.send_message "#{event.user.name} joined!"
-  end
-
-  message(with_text: "!btc") do |event|
-    event.respond "Current Bitcoin Price: #{run_py('btc.py', '')}."
-  end
-end
-
-module UtilityCommands
-  extend Discordrb::Commands::CommandContainer
-
-  @start_time = Time.now
-
-  def self.uptime(start_time)
-    Time.now - start_time
-  end
-
-  command :my_id do |event|
-    event.user.id
-  end
-
-  command :uptime do
-    "I have been running for #{uptime(@start_time)}."
   end
 end
 
